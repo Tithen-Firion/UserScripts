28fd4dcf48f8b62249b07ca0163cebb974b587dd
Tithen-Firion <tithen.firion.0@gmail.com>
2019-12-07 01:39:03 +0100

    [google images paste] add progress bar, add support for www.google.com

--- Google_Images_-_search_by_paste.user.js
+++ Google_Images_-_search_by_paste.user.js
@@ -2,9 +2,10 @@
 // @name        Google Images - search by paste
 // @description Reverse search an image by pasting it
 // @license     MIT
-// @version     1.0.0
+// @version     1.1.0
 // @namespace   tithen-firion.github.io
 // @match       *://images.google.com/*
+// @match       *://www.google.com/*
 // @grant       GM.xmlHttpRequest
 // @grant       GM_xmlhttpRequest
 // @require     https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
@@ -13,14 +14,28 @@
 document.body.addEventListener('paste', e => {
   for(let item of e.clipboardData.items) {
     if(item.type.indexOf('image') > -1) {
+      let progress = document.createElement('div');
+      progress.style.position = 'fixed';
+      progress.style.top = 0;
+      progress.style.left = 0;
+      progress.style.width = '5%';
+      progress.style.height = '5px';
+      progress.style.background = 'green';
+      document.body.appendChild(progress);
+
       let data = new FormData();
-      data.set('encoded_image', item.getAsFile());
+      let file = item.getAsFile();
+      let fileSize = file.size;
+      data.set('encoded_image', file);
       GM.xmlHttpRequest({
         url: 'https://images.google.com/searchbyimage/upload',
         method: 'post',
         data: data,
         onload: response => {
           document.location = response.finalUrl;
+        },
+        onprogress: response => {
+          progress.style.width = response.loaded / fileSize * 100 + '%';
         }
       });
       e.preventDefault();

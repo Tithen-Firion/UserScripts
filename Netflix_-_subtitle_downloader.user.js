4e6d85b39c650fd9f1a470b3997a0ae2dccaf0b4
Tithen-Firion <tithen.firion.0@gmail.com>
2020-04-25 19:18:24 +0200

    [netflix subs] fix the script

--- Netflix_-_subtitle_downloader.user.js
+++ Netflix_-_subtitle_downloader.user.js
@@ -2,7 +2,7 @@
 // @name        Netflix - subtitle downloader
 // @description Allows you to download subtitles from Netflix
 // @license     MIT
-// @version     3.2.1
+// @version     3.2.2
 // @namespace   tithen-firion.github.io
 // @include     https://www.netflix.com/*
 // @grant       unsafeWindow
@@ -245,7 +245,7 @@ const processMessage = e => {
 
 const injection = () => {
   const WEBVTT = 'webvtt-lssdh-ios8';
-  const MANIFEST_URL = "/manifest";
+  const MANIFEST_URL = "manifest";
   const forceSubs = localStorage.getItem('NSD_force-all-lang') !== 'false';
 
   // hijack JSON.parse and JSON.stringify functions
@@ -258,7 +258,7 @@ const injection = () => {
       return data;
     };
     JSON.stringify = function (data) {
-      if (data && data.url === MANIFEST_URL) {
+      if (data && typeof data.url === 'string' && data.url.indexOf(MANIFEST_URL) > -1) {
         for (let v of Object.values(data)) {
         	try {
             if (v.profiles)
